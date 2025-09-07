3475488abc28c760178630c566f7bd9fc2a7b02a
Aamirmalik17 <aamirmalikktd@gmail.com>
2025-09-07 16:29:45 +0530

    Add pipeline.sh demo script

---
+++ pipeline.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "============================="
+echo "Hello from ubuntu "$(date +%F)" "
+echo "Current user "$(whoami)" "
+echo "Currnet directory "$(pwd)" "
+echo  "============================"
